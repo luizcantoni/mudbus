ec582e2ea3b5ec52a261a84411884c60230fa407
Nestor Palomeque <nestorlevi@gmail.com>
2021-11-07 23:57:41 -0300

    Update Mudbus.h
    It allows changing, from the main program, the number of registers, coils and the modbus port. With this you can save RAM memory in the micro.

--- Mudbus/Mudbus.h
+++ Mudbus/Mudbus.h
@@ -29,9 +29,17 @@
 #ifndef Mudbus_h
 #define Mudbus_h
 
+#ifndef MB_N_R
 #define MB_N_R 125 //Max 16 bit registers for Modbus is 125
+#endif
+
+#ifndef MB_N_C
 #define MB_N_C 128 //Max coils for Modbus is 2000 - dont need that many so here is a multiple of 8
+#endif
+
+#ifndef MB_PORT
 #define MB_PORT 502
+#endif
 
 enum MB_FC {
   MB_FC_NONE           = 0,
